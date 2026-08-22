e59b7385a9803245ea4698ead02b5e75acb72012
Dasari Sai Praveen <saidasari5413@gmail.com>
2026-08-22 11:53:22 +0530

    fix(timezone): configure JVM and Jackson timezone to Asia/Kolkata (IST)

--- src/main/java/com/saipraveen/login_registration/LoginRegistrationApplication.java
+++ src/main/java/com/saipraveen/login_registration/LoginRegistrationApplication.java
@@ -1,15 +1,23 @@
 package com.saipraveen.login_registration;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class LoginRegistrationApplication {
 
+	@PostConstruct
+	public void init() {
+		// Set JVM default timezone to Indian Standard Time (IST / Asia/Kolkata)
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+	}
+
 	public static void main(String[] args) {
-		// DataSource is configured in application.properties
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(LoginRegistrationApplication.class, args);
 	}
 
